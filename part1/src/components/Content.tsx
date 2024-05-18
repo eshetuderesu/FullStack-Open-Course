c138fbbc5ae94d138a2b95628e90a3fc306882bb
Eshetu Deresu Disasa <eshetu.deresu3@gmail.com>
2024-05-18 17:01:59 +0300

    part 1 final

---
+++ part1/src/components/Content.tsx
@@ -0,0 +1,7 @@
+const Content = (props) => {
+
+    return (
+           props.parts, props.exercise   
+    )
+}
+export default Content;
---
+++ part1/src/components/Total.tsx
@@ -0,0 +1,9 @@
+const Total = (props) => {
+    return (
+
+        props.exer1 + 
+        props.exer2 + 
+        props.exer3
+    )
+}
+export default Total;
